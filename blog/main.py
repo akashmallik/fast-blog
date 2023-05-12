05e1df8a1ba0af46b6b596d9debdcace315b24cd
akashmallik <toakashmallik@gmail.com>
2023-05-12 08:28:35 +0600

    NO-TICKET: added update and delete operation

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import FastAPI, Depends, Response, HTTPException
 from sqlalchemy.orm import Session
 from starlette import status
@@ -29,14 +31,14 @@ def create(blog: Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get("/blog")
+@app.get("/blog", response_model=List[Blog])
 def create(db: Session = Depends(get_db)):
     blogs = db.query(BlogModel).all()
 
     return blogs
 
 
-@app.get("/blog/{id}")
+@app.get("/blog/{id}", response_model=Blog)
 def create(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(BlogModel).filter(BlogModel.id == id).first()
     if not blog:
@@ -46,3 +48,25 @@ def create(id: int, response: Response, db: Session = Depends(get_db)):
                             detail=f"Blog with the id {id} is not available.")
 
     return blog
+
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    blog = db.query(BlogModel).filter(BlogModel.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="blog with id {id} not found!")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return None
+
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: Blog, db: Session = Depends(get_db)):
+    blog = db.query(BlogModel).filter(BlogModel.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="blog with id {id} not found!")
+    blog.update(request.dict())
+    db.commit()
+    return "updated successfully"
